8847ea8d50cdfcda035c638d7581310076b42310
notnanna <suzanisaac2014@gmail.com>
2024-04-16 10:57:38 +1000

    fixed folder names and started week 5 content

---
+++ week5/script.js
@@ -0,0 +1,13 @@
+const assignment1 = "20";
+const assignment2 = "30";
+const assignment3 = "20";
+
+let total =
+  parseInt(assignment1) + parseInt(assignment2) + parseInt(assignment3);
+console.log("my total", total);
+
+function add() {
+  let a = 30;
+  console.log(a);
+}
+console.log(a);
