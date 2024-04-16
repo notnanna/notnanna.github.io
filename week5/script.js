da25e99cd1b587e2c7b0c4b4af186a76e35d34ba
notnanna <suzanisaac2014@gmail.com>
2024-04-16 12:18:52 +1000

    added week 5 resset and style css

--- week5/script.js
+++ week5/script.js
@@ -1,13 +1,15 @@
-const assignment1 = "20";
-const assignment2 = "30";
-const assignment3 = "20";
-
-let total =
-  parseInt(assignment1) + parseInt(assignment2) + parseInt(assignment3);
-console.log("my total", total);
-
-function add() {
-  let a = 30;
-  console.log(a);
+const body = document.querySelector("body");
+function checkWeather() {
+  let temperature = document.querySelector("#temperature");
+  let temperature = tempt.value;
+  if (temperature < 20 && temperature >= 10) {
+    console.log("it feels cold");
+    body.style.backgroundColor = "lightblue";
+  } else if (temperature >= 20 && temperature < 30) {
+    console.log("it feels warm and sunny");
+  } else if (temperature > 30) {
+    console.log("it is boiling");
+  } else if (temperature < 10) {
+    console.log("it is freezing");
+  }
 }
-console.log(a);
