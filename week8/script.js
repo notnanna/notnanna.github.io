3d2f10171b90378b9c27dbcde578249229627029
notnanna <suzanisaac2014@gmail.com>
2024-05-14 11:58:44 +1000

    Started week 8 content and copied it over to A3

---
+++ week8/script.js
@@ -0,0 +1,50 @@
+// myVideo contains the video I am working with
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+
+// This button will play and pause the video on click
+playPauseButton.addEventListener("click", toggleVideo);
+
+function toggleVideo() {
+  if (myVideo.paused || myVideo.ended) {
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    myVideo.play();
+  } else {
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+    myVideo.pause();
+  }
+}
+
+// This function will muste/unmute the video on click
+const muteUnmuteButton = document.querySelector("#mute-unmute-img");
+console.log(muteUnmuteButton);
+
+muteUnmuteButton.addEventListener("click", toggleSound);
+
+function toggleSound() {
+  if (myVideo.muted) {
+    muteUnmuteButton.style.backgroundColor = "#d5cea3";
+    myVideo.muted = false;
+  } else {
+    muteUnmuteButton.style.backgroundColor = "#736f55";
+    myVideo.muted = true;
+  }
+}
+
+// This function will update the progress bar to show how much of the video is playing and how much time has gone by
+const progressBar = document.querySelector("#progress-bar-fill");
+console.log(progressBar);
+
+myVideo.addEventListener("timeupdate", updateProgressBar);
+
+function updateProgressBar() {
+  const progress = (myVideo.currentTime / myVideo.duration) * 100;
+  console.log(progress);
+  progressBar.style.width = progress + "%";
+}
